ecd4b04688959f4a0a3236e6e9ef3c66e461dbb7
lukemew <lucasmelor65@gmail.com>
2024-08-12 19:54:00 -0300

    Finalizando os métodos e atributos principais das classes Quarto e Inquilino

--- src/Inquilino.java
+++ src/Inquilino.java
@@ -2,6 +2,30 @@ public class Inquilino {
     private String nome;
     private String profissao;
     private int idade;
-    private Quarto quarto;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public String getProfissao() {
+        return this.profissao;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
 
 }
+
--- src/Quarto.java
+++ src/Quarto.java
@@ -2,4 +2,49 @@ public class Quarto {
     private int numero;
     private double valor;
     private Inquilino inquilino;
+
+    public Quarto(Inquilino inquilino) {
+        this.inquilino = inquilino;
+    }
+
+    public Quarto() {
+        this.inquilino = null;
+    }
+
+    public void mostrarDetalhes() {
+        System.out.println("Exibindo informações sobre o quarto " + numero);
+        System.out.println("Valor da diária: " + valor);
+        System.out.println("---");
+        if (inquilino != null) {
+            System.out.println("Exibindo informações do inquilino");
+            System.out.println("Inquilino hospedado: " + inquilino.getNome());
+            System.out.println("Profissão: " + inquilino.getProfissao());
+            System.out.println("Idade: " + inquilino.getIdade());
+            System.out.println("---");
+        } else {
+            System.out.println("Quarto disponível");
+            System.out.println("---");
+        }
+    }
+
+    public void setInquilino(Inquilino inquilino) {
+        this.inquilino = inquilino;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public boolean setDesconto(double desconto) {
+        if(desconto > 0.3) {
+            return false;
+        }
+        this.valor -= this.valor * desconto;
+        return true;
+    }
+
 }
