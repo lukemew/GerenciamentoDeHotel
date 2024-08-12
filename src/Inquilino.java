deb795bc3ea36e39ff45f8c179e95d6a51ad751c
lukemew <lucasmelor65@gmail.com>
2024-08-12 09:40:56 -0300

    Iniciando projeto de Gerenciamento de Hotel

---
+++ src/Inquilino.java
@@ -0,0 +1,7 @@
+public class Inquilino {
+    private String nome;
+    private String profissao;
+    private int idade;
+    private Quarto quarto;
+
+}
---
+++ src/Quarto.java
@@ -0,0 +1,5 @@
+public class Quarto {
+    private int numero;
+    private double valor;
+    private Inquilino inquilino;
+}
